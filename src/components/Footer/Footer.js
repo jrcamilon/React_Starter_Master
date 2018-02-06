0cfb4e63cf104aa56902d229ead20bf14566b286
Eliseo Camilon <ecamilon@asu.edu>
2018-02-06 11:39:36 -0700

    Test
    Test

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="app-footer">
+        <span><a href="http://coreui.io">InfoSol, Inc</a> &copy; 2018 Infosol Bii</span>
+        <span className="ml-auto">Powered by <a href="http://coreui.io">InfoBurst</a></span>
+      </footer>
+    )
+  }
+}
+
+export default Footer;
---
+++ src/components/Sidebar/_nav.js
@@ -0,0 +1,18 @@
+export default {
+  items: [
+    {
+      name: 'Dashboard',
+      url: '/dashboard',
+      icon: 'icon-speedometer',
+      badge: {
+        variant: 'info'
+        // text: 'NEW'
+      }
+    }, 
+    {
+      name: "Charts",
+      url: "/charts",
+      icon: "icon-pie-chart"
+    }
+  ]
+};
